d1828d0c5db1a52f01c355ad8bdcee31122d338d
Mohamed Gado <boogado@yahoo.com>
2022-10-04 15:53:20 +0200

    final upload

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-const { app, BrowserWindow, Menu } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron')
+const resizeImg = require('resize-img')
 
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
+
+process.env.NODE_ENV = 'production'
 const isMac = process.platform === 'darwin'
 const isDev = process.env.NODE_ENV !== 'production'
 
+let mainWindow
 // main screen
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'Image Resizer',
     width: isDev ? 1000 : 500,
     height: 600,
     resizable: false,
     webPreferences: {
+      preload: path.join(__dirname, './preload.js'),
       nodeIntegration: true,
+      contextIsolation: true,
     },
   })
 
@@ -45,6 +53,9 @@ app.whenReady().then(() => {
   //   implement menu
   const mainMenu = Menu.buildFromTemplate(menu)
   Menu.setApplicationMenu(mainMenu)
+
+  //   remove main window on close
+  mainWindow.on('closed', () => (mainWindow = null))
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -93,6 +104,40 @@ const menu = [
     : []),
 ]
 
+// respond to ipcRenderer resize
+ipcMain.on('image:resize', (e, options) => {
+  options.dest = path.join(os.homedir(), 'imageresizer')
+  resizeImage(options)
+})
+
+// resize image
+async function resizeImage({ imgPath, width, height, dest }) {
+  try {
+    const newPath = await resizeImg(fs.readFileSync(imgPath), {
+      width: +width,
+      height: +height,
+    })
+    //    file name
+    const filename =
+      path.basename(imgPath, path.extname(imgPath)) +
+      Date.now() +
+      path.extname(imgPath)
+
+    //   create directory if not exist
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest)
+    }
+
+    // write file to dest
+    fs.writeFileSync(path.join(dest, filename), newPath)
+
+    // send success message to renderer
+    mainWindow.webContents.send('image:done')
+
+    //open dest folder
+    shell.openPath(dest)
+  } catch (error) {}
+}
 app.on('window-all-closed', () => {
   if (!isMac) {
     app.quit()
